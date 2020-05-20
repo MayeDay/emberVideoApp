558aece571b39a90e5666ea5b3ddb5ec45c1e633
James <Jamesmaye6094@gmail.com>
2020-05-21 02:11:24 +0800

    Adding Footer

--- app/components/footer-section-comp.js
+++ app/components/footer-section-comp.js
@@ -7,19 +7,32 @@ export default class FooterSectionCompComponent extends Component {
             name: "Personal",
             information: [{
                 information_1 : "Facebook",
-                information_2 :  "Instagram",
+                information_2 : "Instagram",
                 information_3 : "Twitter",
                 information_4 : "GitHub",
-                information_5 : "Linkdin"
+                information_5 : "Linkdin",
+
+                link_1 : "https://www.facebook.com/OriginalMaye",
+                link_2 : "https://www.instagram.com/0riginal_maye/?hl=en",
+                link_3 : "https://twitter.com/ThyFilthyNoob",
+                link_4 : "https://github.com/MayeDay",
+                link_5: "https://www.linkedin.com/in/james-maye-b1b944139/"
             }]
         },{
             name: "Projects",
             information: [{
                 information_1 : "Java Game Dev",
-                information_2 : "Python Coding",
+                information_2 : "Javascript Coding",
                 information_3 : "Ember.js Projects",
                 information_4 : "Website Designs",
-                information_5 : "MySQL Database Designs"
+                information_5 : "MySQL Database Designs",
+
+                link_1 : "https://github.com/MayeDay/First-Attempt-at-GameMaking",
+                link_2 : "https://github.com/MayeDay/Javascript-Form",
+                link_3 : "https://github.com/MayeDay/EmberPractice",
+                link_4 : "https://github.com/MayeDay/DaddyWaddy",
+                link_5 : "https://github.com/MayeDay/Airline-System"
+
             }]
         }
 
