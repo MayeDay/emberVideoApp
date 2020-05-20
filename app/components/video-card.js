acf94c4dcf07725b751b066d827f9712a5b080b9
James <Jamesmaye6094@gmail.com>
2020-05-20 21:33:31 +0800

    Adding Videos

---
+++ app/components/video-card.js
@@ -0,0 +1,24 @@
+import Component from '@glimmer/component';
+
+export default class VideoCardComponent extends Component {
+
+    model =  [
+        {
+            title: "Ember DND Helper",
+            people: "Taras Mankovski",
+            link: "peNV2yJRMLo?rel=0"
+        },{
+            title: "Dependency Injection in Angular 2",
+            people: "Patrick J. Stapleton",
+            link: "46WovCX8i-I?rel=0"
+        },{
+            title: "Angular CLI",
+            people: "Mike Brocchi",
+            link: "BmZLpNRNnZo"
+        },{
+            title: "Angular Material 2 Spelunking & Issue Submission",
+            people: "Ben Lesh",
+            link: "3gNsyL7wpXU"
+        }
+    ];
+}
